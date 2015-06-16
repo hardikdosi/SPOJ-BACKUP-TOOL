d2ce5c42a55d3f9d5530fbb138236807b976bf8b
Hardik Dosi <hardikdosi@users.noreply.github.com>
2015-06-16 23:45:20 +0530

    Update SPOJ AC Solutions Download Script.py

--- SPOJ AC Solutions Download Script.py
+++ SPOJ AC Solutions Download Script.py
@@ -1,6 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+
+             _____ _____ _____    __    _           _
+            |   __|  _  |     |__|  |  | |_ ___ ___| |_ _ _ ___
+            |__   |   __|  |  |  |  |  | . | .'|  _| '_| | | . |
+            |_____|__|  |_____|_____|  |___|__,|___|_,_|___|  _|
+                                                           |_|
+                                    by Abhishek Mishra      <ideamonk@gmail.com >
+                                       Shashwat Anand  <anand.shashwat@gmail.com> 
+                                    
+                                    Fixed By: Hardik Dosi <hardik0895@gmail.com>
+                                    
+
+Keywords: python, tools, algorithms, spoj
+
+Copyright (C) 2003-2004 Free Software Foundation, Inc.
+
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of the Secret Labs AB nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
 # Dependencies:
 # mechanize => http://pypi.python.org/pypi/mechanize/0.1.7b
 
